0ab4d718a19ff355fc07439f7af3fc8cf22f409c
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-08-25 02:20:11 +0530

    secrets!!

--- validation.php
+++ validation.php
@@ -14,8 +14,8 @@ if(isset($_POST['submit']))
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'projecttestbot@gmail.com';                 // SMTP username
-        $mail->Password = 'SAN24ka1996lpprojectbot';                           // SMTP password
+        $mail->Username = '';                 // SMTP username
+        $mail->Password = '';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
 
